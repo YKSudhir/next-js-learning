7737db9e4be17b203c7071cf26fba1f20c1391d7
Sudhir Kumar Yadav <145853393+YKSudhir@users.noreply.github.com>
2024-12-07 18:14:03 +0530

    First commit ch-5 done

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export { inter, lusitana };
